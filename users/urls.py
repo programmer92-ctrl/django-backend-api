9e07c9b9e9c097ed023eb5500732f10ca52b9713
programmer92-ctrl <computerdev1992@gmail.com>
2025-02-03 13:32:01 -0700

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
 
     path('user/register', views.register, name='register'),
     path('user/get_user_by_username/<str:username>', views.get_user_by_username, name='get_user_by_username'),
-    path('user/login', views.login, name='login),
+    path('user/login', views.login, name='login'),
 
 ]
