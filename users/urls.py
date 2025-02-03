ee10e35eb7eac1622bc76aca04bb6e4a083bb1b6
programmer92-ctrl <computerdev1992@gmail.com>
2025-02-03 11:11:31 -0700

    Create urls.py

---
+++ users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+    path('user/register', views.register, name='register'),
+    path('user/get_user_by_username/<str:username>', views.get_user_by_username, name='get_user_by_username'),
+    path('user/login', views.login, name='login),
+
+]
